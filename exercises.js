c0572e37808e34bb42e1a347627f78160c826024
doncamarillo <don_camarillo@yahoo.com>
2024-10-28 06:35:39 -0700

    completed exercise 4

--- exercises.js
+++ exercises.js
@@ -14,11 +14,33 @@ console.log(multipliedNums)
 
 // Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
 
+// const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// // Your code here
+
+// const [firstTopping, secondTopping, thirdTopping] = pizzaToppings
+// console.log (firstTopping)
+// console.log (secondTopping)
+// console.log (thirdTopping)
+
+// Given the following object, use destructuring to create variables `make` and `model` that will hold the respective values.
+
+const car = {
+    make: 'Audi',
+    model: 'q5',
+  };
+  
+  // Your code here
+
+  const {make,model} = car
+  console.log (make)
+  console.log (model)
+
+  // Duplicate the following array using the spread operator and assign it to `controversialPizzaToppings`. Then, log the variable.
+
 const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
 
 // Your code here
 
-const [firstTopping, secondTopping, thirdTopping] = pizzaToppings
-console.log (firstTopping)
-console.log (secondTopping)
-console.log(thirdTopping)
+const toppingsDupe = [...pizzaToppings, 'pepperoni', 'ham']
+console.log (toppingsDupe)
